2e8be44d158b6a4d115b5200612a8be90cc040e8
MJW980903 <73779660+MJW980903@users.noreply.github.com>
2021-03-19 10:57:53 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -45,9 +45,9 @@ if os.environ.get('GITHUB_RUN_ID', None):
 
     smtp_port = os.environ.get('SMTP_PORT', '465')  # 邮件服务器端口，默认为qq smtp服务器端口
     smtp_server = os.environ.get('SMTP_SERVER', 'smtp.qq.com')  # 邮件服务器，默认为qq smtp服务器
-    sender_email = os.environ.get('SENDER_EMAIL', '470683035@qq.com')  # 发送通知打卡通知邮件的邮箱
-    sender_email_passwd = os.environ.get('SENDER_EMAIL_PASSWD', "password")  # 发送通知打卡通知邮件的邮箱密码
-    receiver_email = os.environ.get('RECEIVER_EMAIL', '470683035@qq.com')  # 接收打卡通知邮件的邮箱
+    sender_email = os.environ.get('SENDER_EMAIL', '')  # 发送通知打卡通知邮件的邮箱
+    sender_email_passwd = os.environ.get('SENDER_EMAIL_PASSWD', "")  # 发送通知打卡通知邮件的邮箱密码
+    receiver_email = os.environ.get('RECEIVER_EMAIL', '')  # 接收打卡通知邮件的邮箱
 
 
 def login(s: requests.Session, username, password, cookie_file: Path):
